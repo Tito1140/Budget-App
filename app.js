4df41d75e3ef24c5eb36d075a21a41cbc76effa2
John Tower <juan.c.torres1140@gmail.com>
2019-09-16 22:13:28 -0400

    Enter button event listener

--- app.js
+++ app.js
@@ -7,14 +7,35 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function () {
-        console.log('Button was clicked');
-    });
+    var ctrlAddItem = function () {
+
+        // 1. Get the filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
+
+
