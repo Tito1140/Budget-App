40373af3f3c5b89495ad1eb2b3f4059b1ad20b76
John Tower <juan.c.torres1140@gmail.com>
2019-09-16 21:53:42 -0400

    starting html and css for budget app

---
+++ app.js
@@ -0,0 +1,20 @@
+// BUDGET CONTROLLER
+var budgetController = (function () {
+
+
+})();
+
+// UI CONTROLLER
+var UIController = (function () {
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
+
+    document.querySelector('.add__btn').addEventListener('click', function () {
+        console.log('Button was clicked');
+    });
+
+})(budgetController, UIController);
